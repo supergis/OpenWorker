36aba08f1b9a835b17b508f178d11fb0f5b20659
supergis <wangeq33@gmail.com>
2015-05-29 10:13:20 +0800

    commit openworker updated.

--- commit.sh
+++ commit.sh
@@ -7,18 +7,10 @@ echo "OpenWorker-Open Worker Framework to process data parallelly."
 echo "基于Python的并行处理框架，集成Celery、Flower、Jobtastic和Rodeo工程."
 echo "================================================================="
 
-cd ..
-echo "1、并行处理框架-Celery"
-git clone https://github.com/celery/celery.git
 
-echo "2、Web管理控制台-Flower"
-git clone https://github.com/mher/flower.git
+git add ./ -A
+git commit -a -m "commit openworker updated."
+git push
 
-echo "3、任务进度通知-Jobtastic"
-git clone https://github.com/PolicyStat/jobtastic.git
-
-echo "4、Web上Python控制台-Rodeo"
-git clone https://github.com/yhat/rodeo.git
-
-echo "======OpenWorker needed project now git cloned.================="
+echo "======OpenWorker commited and pushed to Github.com.==============="
 
